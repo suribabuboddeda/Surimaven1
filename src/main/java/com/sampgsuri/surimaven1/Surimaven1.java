fd756efb266d428a15d9294a28be0a6a770203af
Suri Babu Boddeda (sboddeda) <sboddeda@cisco.com>
2023-02-27 20:07:21 +0530

    initial commit

---
+++ src/main/java/com/sampgsuri/surimaven1/Surimaven1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.sampgsuri.surimaven1;
+
+/**
+ *
+ * @author sboddeda
+ */
+public class Surimaven1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
